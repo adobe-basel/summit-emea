2a166be14e1e744378d29ca231b80b2d621efd1e
Dominik Suess <suess@adobe.com>
2023-05-10 09:53:57 +0200

    lint fixes

--- blocks/sponsors/sponsors.js
+++ blocks/sponsors/sponsors.js
@@ -1,25 +1,25 @@
 export default function decorate(block) {
-    block.querySelectorAll('a').forEach((a) => {
-        a.title = a.title || a.textContent;
-        if (a.href !== a.textContent) {
-          const up = a.parentElement;
-          const twoup = a.parentElement.parentElement;
-          if (!a.querySelector('img')) {
-            if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-              a.className = ''; // default
-              up.classList.remove('button-container');
-            }
-            if (up.childNodes.length === 1 && up.tagName === 'STRONG'
-              && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-                a.className = ''; // default
-                up.classList.remove('button-container');
-            }
-            if (up.childNodes.length === 1 && up.tagName === 'EM'
-              && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-                a.className = ''; // default
-                twoup.classList.remove('button-container');
-            }
-          }
+  block.querySelectorAll('a').forEach((a) => {
+    a.title = a.title || a.textContent;
+    if (a.href !== a.textContent) {
+      const up = a.parentElement;
+      const twoup = a.parentElement.parentElement;
+      if (!a.querySelector('img')) {
+        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
+          a.className = ''; // default
+          up.classList.remove('button-container');
         }
-      });
+        if (up.childNodes.length === 1 && up.tagName === 'STRONG'
+          && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
+          a.className = ''; // default
+          up.classList.remove('button-container');
+        }
+        if (up.childNodes.length === 1 && up.tagName === 'EM'
+          && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
+          a.className = ''; // default
+          twoup.classList.remove('button-container');
+        }
+      }
+    }
+  });
 }
